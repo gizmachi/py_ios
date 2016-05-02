85ca65e426764656d024d97b4b772fe24fdbe6a2
Per Josef Felix Gustafsson (perjgust) <perjgust@cisco.com>
2016-05-02 14:25:21 +0200

    refactoring and adding macros

--- pyios.py
+++ pyios.py
@@ -2,6 +2,12 @@ import socket, select, string, sys, thread, time
 from socket import inet_ntoa
 from struct import pack
 
+## Some default parameters
+default_netmask = "24"
+default_username = "josef"
+default_password = "cisco"
+default_domain = "example.com"
+
 def thread_send(s):
     while True:
         msg = sys.stdin.readline()
@@ -17,7 +23,7 @@ def thread_rec(threadname, s):
             sys.stdout.write(data)
         
 def cidr2ddn(mask):
-    bits = 0xffffffff ^ (1 << 32 - mask) - 1
+    bits = 0xffffffff ^ (1 << 32 - int(mask)) - 1
     return inet_ntoa(pack('>I', bits))
 
 def setupSocket():    
@@ -49,14 +55,22 @@ def macro_save(s):
     s.send("copy run start" + "\r\n")
     s.send("\r\n")
 
-def macro_interface(s, inf, ip, mask="24"):
+def macro_interface(s, interface, ipaddr, mask=default_netmask):
     if len(mask) < 7:
-        mask = cidr2ddn(mask)
+        mask = cidr2ddn(int(mask))
     
     s.send("conf t" + "\r\n")
     s.send("interface " + interface + "\r\n")
-    s.send("ip address " + ipaddr + " " + netmask + "\r\n")
+    s.send("ip address " + ipaddr + " " + mask + "\r\n")
     s.send("no shutdown" + "\r\n")
+    macro_save(s)
+
+def macro_setup(s):
+    s.send("conf t" + "\r\n")
+    s.send("no ip domain-lookup" + "\r\n")
+    s.send("service password-encryption" + "\r\n")
+    s.send("enable secret " + default_password + "\r\n")
+    s.send("username " + default_username + " privilege 15 secret " + default_password + "\r\n")
     macro_save(s)
 
 
@@ -74,12 +88,18 @@ if __name__ == "__main__":
         line = sys.stdin.readline()
         splitline = line.split()
         if len(splitline) > 0 :
+
+            # Exit
             if splitline[0] == "kill":
                 print "exiting..."
                 sys.exit()
+
+            # Macro
             elif splitline[0] == 'macro':
                 if len(splitline) == 2:
                     print "No marco specified!"
+
+                # Setup interface with IPv4 address
                 if splitline[1] == 'int':
                     if len(splitline) == 5:
                         macro_interface(s, splitline[2], splitline[3],splitline[4])
@@ -87,8 +107,24 @@ if __name__ == "__main__":
                         macro_interface(s, splitline[2], splitline[3])
                     else:
                         print "wrong nr of arguments!"
+
+                # setup some default settings for device
+                elif splitline[1] == 'setup':
+                    macro_setup(s)
+
+                # Save running configuration to startup configuration
+                elif splitline[1] == 'save':
+                    macro_save(s)
+
+                # specific macro not found
+                else:
+                    print "MACRO NOT IMPLEMENTED!"
+
+            # Regular command line
             else:
                 s.send(line + "\r\n")
+        
+        # Empty newline
         else:
             s.send(line + "\r\n")
             
