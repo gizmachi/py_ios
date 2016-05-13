41d5ff3d891babff6c3a2c8d9160020c3ebd2811
Josef Gustafsson (perjgust) <perjgust@cisco.com>
2016-05-13 16:16:06 +0200

    connecting to all running GNS devices

--- pyios.py
+++ pyios.py
@@ -8,12 +8,30 @@ default_username = "josef"
 default_password = "cisco"
 default_domain = "example.com"
 
+
+class config:
+    hostname = None
+    socket = None
+
+    def __init__(self, name):
+        self.hostname = name
+
+
+class device:
+    hostname = None
+    idnr = None
+    def __init__(self, id):
+        self.hostname = "NAME_NOT_SET"
+        self.idnr = id
+
+
+
 def thread_send(s):
     while True:
         msg = sys.stdin.readline()
         s.send(msg + "\r\n")
 
-def thread_rec(threadname, s):
+def thread_rec(conf, s):
     while True:
         data = s.recv(4096)
         if not data :
@@ -21,14 +39,19 @@ def thread_rec(threadname, s):
             sys.exit()
         else :
             sys.stdout.write(data)
+            if "hostname" in data:
+                conf.hostname = data.split()[-1]
+
         
 def cidr2ddn(mask):
     bits = 0xffffffff ^ (1 << 32 - int(mask)) - 1
     return inet_ntoa(pack('>I', bits))
 
-def setupSocket():    
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+def setupSocket(conf, host=None, port=None):
+    if host == None:    
+        host = sys.argv[1]
+    if port == None:
+        port = int(sys.argv[2])
      
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(100000)
@@ -38,16 +61,41 @@ def setupSocket():
         s.connect((host, port))
     except :
         print 'Unable to connect'
-        sys.exit()
-     
-    print 'Connected to remote host'
+        #sys.exit()
+        return None
+
+    #print 'Connected to remote host'
 
     socket_list = [s]
     read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
     ThreadName = "sending_thread"
 
-    rt = thread.start_new_thread(thread_rec, (ThreadName, s))
+    rt = thread.start_new_thread(thread_rec, (conf, s))
     return s
+
+def connect():
+    print "looking for GNS devices..."
+
+    devices = []
+
+    conf = config("NAME_NOT_SET")
+    address = "localhost"
+    port = 2000
+    dev_id = 1
+
+    while True:
+        sock = setupSocket(conf, address, port)
+        if sock is not None:
+            newDevice = device(dev_id)
+            newDevice.socket = sock
+            devices.append(newDevice)
+            port += 1
+            dev_id += 1
+        else:
+            #print "Found " + str(len(devices)) + " running devices."
+            return devices
+
+
 
 ### MACROs
 def macro_save(s):
@@ -77,12 +125,20 @@ def macro_setup(s):
 #main function
 if __name__ == "__main__":
      
-    if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
+    #if(len(sys.argv) < 3) :
+    #    print 'Usage : python telnet.py hostname port'
+    #    sys.exit()
+
+    conf = config("NAME_NOT_SET")
+    #s = setupSocket(conf)
+    dev = connect()
+
+    if len(dev) > 0:
+        print "Found " + str(len(dev)) + " running devices.."
+        s = dev[0].socket
+    else:
+        print "No devices found"
         sys.exit()
-
-    s = setupSocket()
-
 
     while True:
         line = sys.stdin.readline()
@@ -96,7 +152,7 @@ if __name__ == "__main__":
 
             # Macro
             elif splitline[0] == 'macro':
-                if len(splitline) == 2:
+                if len(splitline) == 1:
                     print "No marco specified!"
 
                 # Setup interface with IPv4 address
@@ -115,6 +171,9 @@ if __name__ == "__main__":
                 # Save running configuration to startup configuration
                 elif splitline[1] == 'save':
                     macro_save(s)
+
+                elif splitline[1] == 'hostname':
+                    print "Hostname: " + conf.hostname + "\n"
 
                 # specific macro not found
                 else:
